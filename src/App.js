a608bdcc0bd317505fd3a5861b2ba722213078e9
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2022-09-16 15:04:14 +0200

    tutorial for todolist finished

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <header>
+        <h1>Todo app</h1>
+      </header>
+      <TodoList/>
+
+    </div>
+  );
+}
+
+export default App;
